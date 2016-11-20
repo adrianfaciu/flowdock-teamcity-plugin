ae801975061393eba6b63a008de583c6bf2716fc
Adrian Fâciu <adrian.faciu@gmail.com>
2016-11-20 12:39:13 +0200

    Adding settings manager

--- flowdock-teamcity-plugin-server/src/main/kotlin/com/adrianfaciu/teamcity/flowdockPlugin/core/FlowdockManager.kt
+++ flowdock-teamcity-plugin-server/src/main/kotlin/com/adrianfaciu/teamcity/flowdockPlugin/core/FlowdockManager.kt
@@ -2,27 +2,30 @@ package com.adrianfaciu.teamcity.flowdockPlugin.core
 
 import com.adrianfaciu.teamcity.flowdockPlugin.notifications.FlowdockNotification
 import com.adrianfaciu.teamcity.flowdockPlugin.notifications.NotificationType
+import com.adrianfaciu.teamcity.flowdockPlugin.util.SettingsManager
 import com.adrianfaciu.teamcity.flowdockPlugin.util.logInfoMessage
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelManager
 import com.google.gson.GsonBuilder
 
 class FlowdockManager {
-    fun sendNotification(notification: FlowdockNotification){
-        var url = "https://api.flowdock.com/messages"
+    private val settingsManager = SettingsManager()
 
+    fun sendNotification(notification: FlowdockNotification){
         val builder = GsonBuilder()
         val gson = builder.create()
         var messageBody = gson.toJson(notification)
 
         FuelManager.instance.baseHeaders = mapOf("Content-Type" to "application/json")
-        var response = Fuel.post(url).body(messageBody).response()
+        var response = Fuel.post(settingsManager.flowdockApiEndpoint)
+                           .body(messageBody)
+                           .response()
 
         logInfoMessage(response.toString())
     }
 
     fun shouldSend(type: NotificationType): Boolean {
         // TODO check if we want/need to send this notification
-        return true;
+        return true
     }
 }
--- flowdock-teamcity-plugin-server/src/main/kotlin/com/adrianfaciu/teamcity/flowdockPlugin/core/NotificationBuilder.kt
+++ flowdock-teamcity-plugin-server/src/main/kotlin/com/adrianfaciu/teamcity/flowdockPlugin/core/NotificationBuilder.kt
@@ -4,15 +4,19 @@ import com.adrianfaciu.teamcity.flowdockPlugin.notifications.FlowdockNotificatio
 import com.adrianfaciu.teamcity.flowdockPlugin.notifications.NotificationAuthor
 import com.adrianfaciu.teamcity.flowdockPlugin.notifications.NotificationThread
 import com.adrianfaciu.teamcity.flowdockPlugin.notifications.NotificationType
+import com.adrianfaciu.teamcity.flowdockPlugin.util.SettingsManager
 import jetbrains.buildServer.serverSide.SRunningBuild
 import jetbrains.buildServer.users.SUser
 
 class NotificationBuilder {
+    private val settingsManager = SettingsManager()
+
     fun createNotification(type: NotificationType, build: SRunningBuild, users: MutableSet<SUser>): FlowdockNotification {
         var notification =  FlowdockNotification()
-        notification.flow_token = "e9ce795411351be641ad845a4b910289"
+        notification.flow_token = settingsManager.getFlowToken()
         notification.event = "activity"
         notification.author = NotificationAuthor("Adrian","https://avatars.githubusercontent.com/u/3017123?v=3", null)
+
         notification.title = "Build event"
         notification.body = "message from TC"
         notification.external_thread_id= "build"
